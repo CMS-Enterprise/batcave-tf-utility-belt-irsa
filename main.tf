eb9e4fbb8a16b0dac5750361708c858ad70bc150
Srikanth Kumbala <117395141+skumbala40@users.noreply.github.com>
2024-01-16 13:25:39 -0500

    BATIAI-2213-Migrate the rest of the IRSA modules into batcave-tf-utility-belt-irsa (#3)
    * adding rest of the irsa modules
    
    * adding rest of the irsa modules
    
    * updated the code based on PR comments
    
    * updated the code based on PR comments
    
    * updated the code based on PR comments
    
    * [pre-commit.ci lite] apply automatic fixes
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -48,3 +48,106 @@ module "velero_irsa" {
   }
 
 }
+
+module "argocd_irsa" {
+  source                        = "git::git@github.com:CMS-Enterprise/batcave-tf-irsa.git//.?ref=1.0.0"
+  role_name                     = "${var.cluster_name}-ub-argocd"
+  role_path                     = var.iam_path
+  role_permissions_boundary_arn = var.permissions_boundary
+  app_name                      = var.argocd_app_name
+  sops_arn                      = var.argocd_sops_arn
+  attach_sops_policy            = true
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = var.argocd_service_accounts
+    }
+  }
+
+}
+
+module "fluentbit_irsa" {
+  source                        = "git::git@github.com:CMS-Enterprise/batcave-tf-irsa.git//.?ref=1.0.0"
+  role_name                     = "${var.cluster_name}-ub-fluentbit"
+  role_path                     = var.iam_path
+  role_permissions_boundary_arn = var.permissions_boundary
+  app_name                      = var.fluentbit_app_name
+  s3_bucket_arns                = var.fluentbit_bucket_arns
+  attach_s3_policy              = true
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = var.fluentbit_service_accounts
+    }
+  }
+
+}
+
+module "flux_irsa" {
+  source                        = "git::git@github.com:CMS-Enterprise/batcave-tf-irsa.git//.?ref=1.0.0"
+  role_name                     = "${var.cluster_name}-ub-flux"
+  role_path                     = var.iam_path
+  role_permissions_boundary_arn = var.permissions_boundary
+  app_name                      = var.flux_app_name
+  sops_arn                      = var.flux_sops_arn
+  attach_sops_policy            = true
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = var.flux_service_accounts
+    }
+  }
+
+}
+
+module "container-insights_irsa" {
+  source                        = "git::git@github.com:CMS-Enterprise/batcave-tf-irsa.git//.?ref=1.0.0"
+  role_name                     = "${var.cluster_name}-ub-container-insights"
+  role_path                     = var.iam_path
+  role_permissions_boundary_arn = var.permissions_boundary
+  app_name                      = var.container-insights_app_name
+  attach_insights_policy        = true
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = var.container-insights_service_accounts
+    }
+  }
+
+}
+
+module "gitlab_irsa" {
+  for_each                      = toset(length(var.gitlab_runner_bucket_arns) > 0 ? ["create"] : [])
+  source                        = "git::git@github.com:CMS-Enterprise/batcave-tf-irsa.git//.?ref=1.0.0"
+  role_name                     = "${var.cluster_name}-ub-gitlab"
+  role_path                     = var.iam_path
+  role_permissions_boundary_arn = var.permissions_boundary
+  app_name                      = var.gitlab_app_name
+  s3_bucket_arns                = var.gitlab_bucket_arns
+  attach_s3_policy              = true
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = var.gitlab_service_accounts
+    }
+  }
+
+}
+
+module "gitlab_runner_irsa" {
+  for_each                      = toset(length(var.gitlab_runner_bucket_arns) > 0 ? ["create"] : [])
+  source                        = "git::git@github.com:CMS-Enterprise/batcave-tf-irsa.git//.?ref=1.0.0"
+  role_name                     = "${var.cluster_name}-ub-gitlab-runner"
+  role_path                     = var.iam_path
+  role_permissions_boundary_arn = var.permissions_boundary
+  app_name                      = var.gitlab_runner_app_name
+  s3_bucket_arns                = var.gitlab_runner_bucket_arns
+  attach_s3_policy              = true
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = var.gitlab_runner_service_accounts
+    }
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,137 @@
+
+variable "iam_path" {
+  description = "Path of IAM role"
+  type        = string
+  default     = "/delegatedadmin/developer/"
+}
+
+variable "permissions_boundary" {
+  description = "Permissions boundary ARN to use for IAM role"
+  type        = string
+}
+variable "oidc_provider_arn" {
+  type = string
+}
+variable "cluster_name" {
+  type = string
+}
+variable "loki_bucket_arns" {
+  type        = list(string)
+  description = "buckets for Loki"
+}
+variable "loki_app_name" {
+  type    = string
+  default = "loki"
+}
+variable "loki_service_accounts" {
+  type    = list(string)
+  default = ["logging:logging-loki"]
+}
+variable "thanos_bucket_arns" {
+  type        = list(string)
+  description = "buckets for thanos"
+}
+variable "thanos_app_name" {
+  type    = string
+  default = "thanos"
+}
+variable "thanos_service_accounts" {
+  type = list(string)
+  default = [
+    "monitoring:thanos-storegateway",
+    "monitoring:prometheus",
+  ]
+}
+
+variable "velero_bucket_arns" {
+  type        = list(string)
+  description = "buckets for velero"
+}
+variable "velero_service_accounts" {
+  type    = list(string)
+  default = ["velero:velero-velero-server"]
+}
+
+variable "argocd_sops_arn" {
+  type        = string
+  description = "sops kms key arn for argocd"
+}
+variable "argocd_app_name" {
+  type    = string
+  default = "ArgoCD"
+}
+variable "argocd_service_accounts" {
+  type = list(string)
+  default = [
+    "argocd:argocd-application-controller",
+    "argocd:argocd-argocd-repo-server",
+    "argocd:argocd-server"
+  ]
+}
+
+variable "fluentbit_bucket_arns" {
+  type        = list(string)
+  description = "buckets for fluentbit"
+}
+variable "fluentbit_app_name" {
+  type    = string
+  default = "Fluentbit"
+}
+variable "fluentbit_service_accounts" {
+  type    = list(string)
+  default = ["fluentbit:fluentbit-fluent-bit"]
+}
+
+variable "flux_sops_arn" {
+  type        = string
+  description = "sops kms key arn for flux"
+}
+variable "flux_app_name" {
+  type    = string
+  default = "Flux"
+}
+variable "flux_service_accounts" {
+  type    = list(string)
+  default = ["flux-system:kustomize-controller"]
+}
+
+
+variable "container-insights_app_name" {
+  type    = string
+  default = "container-insights"
+}
+variable "container-insights_service_accounts" {
+  type    = list(string)
+  default = ["amazon-cloudwatch:aws-cloudwatch-metrics"]
+}
+
+variable "gitlab_bucket_arns" {
+  type        = list(string)
+  description = "buckets for gitlab"
+}
+variable "gitlab_app_name" {
+  type    = string
+  default = "GitLab"
+}
+variable "gitlab_service_accounts" {
+  type = list(string)
+  default = [
+    "gitlab:gitlab-minio",
+    "gitlab:gitlab-webservice",
+    "gitlab:gitlab-toolbox",
+    "gitlab:gitlab-sidekiq"
+  ]
+}
+
+variable "gitlab_runner_bucket_arns" {
+  type        = list(string)
+  description = "buckets for gitlab_runner"
+}
+variable "gitlab_runner_app_name" {
+  type    = string
+  default = "GitlabRunner"
+}
+variable "gitlab_runner_service_accounts" {
+  type    = list(string)
+  default = ["gitlab-runner:gitlab-runner"]
+}
