67c030c68a98c18a6160ef450ae0f92780bc3201
shanawazeshaik <106613833+sshaikRC@users.noreply.github.com>
2024-02-22 15:50:53 -0500

    BATIAI-2296 - adding irsa for external secrets (#9)
    * adding irsa for external secrets
    
    * [pre-commit.ci lite] apply automatic fixes
    
    ---------
    
    Co-authored-by: Shanawaze <shanawaze.shaik@revacomm.com>
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -154,3 +154,10 @@ variable "grafana_service_accounts" {
     "monitoring:monitoring-monitoring-prometheus-node-exporter"
   ]
 }
+
+variable "external_secrets_service_accounts" {
+  type = list(string)
+  default = [
+    "external-secrets:imagepull-es"
+  ]
+}
