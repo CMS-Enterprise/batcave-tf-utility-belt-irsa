ec335c5514e27b380aef5de0b93089397808bd64
Shanawaze <shanawaze.shaik@revacomm.com>
2024-02-22 10:35:27 -0600

    adding irsa for external secrets

--- main.tf
+++ main.tf
@@ -170,3 +170,20 @@ module "grafana_irsa" {
   }
 
 }
+
+module "external_secrets_irsa" { 
+  source                        = "git::git@github.com:CMS-Enterprise/batcave-tf-irsa.git//.?ref=1.0.1"
+  role_name                     = "${var.cluster_name}-ub-external-secrets"
+  role_path                     = var.iam_path
+  role_permissions_boundary_arn = var.permissions_boundary
+  app_name                      = "ESO"
+  attach_secretsmanager_policy  = true
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = var.external_secrets_service_accounts
+    }
+  }
+
+
+}
